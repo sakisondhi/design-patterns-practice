95f34672b558c0917bb1e5a9e7eb124648f9f810
sakshamsondhi <sakshamsondhi1996@gmail.com>
2023-04-16 17:22:46 +0530

    initialized by JVM

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,12 +1,9 @@
 public class Singleton {
-    private static Singleton uniqueInstance;
+    private static Singleton uniqueInstance = new Singleton();;
 
     private Singleton(){}
 
     public static synchronized Singleton getInstance(){
-        if(uniqueInstance == null){
-            uniqueInstance = new Singleton();
-        }
         return uniqueInstance;
     }
 }
