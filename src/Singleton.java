7f4bb413f1d6a1b2564b6927ae269d8273eed7b2
sakshamsondhi <sakshamsondhi1996@gmail.com>
2023-04-16 17:24:49 +0530

    Double checked locking

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,9 +1,16 @@
 public class Singleton {
-    private static Singleton uniqueInstance = new Singleton();;
+    private static volatile Singleton uniqueInstance;
 
     private Singleton(){}
 
     public static synchronized Singleton getInstance(){
+        if(uniqueInstance == null){
+            synchronized (Singleton.class){
+                if(uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
         return uniqueInstance;
     }
 }
